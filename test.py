940609b792177e75fd4b4e1ca49a55f7c671d8ff
yulinhu1129 <yulinhu@umich.edu>
2022-12-01 22:05:06 -0500

    add test script

---
+++ test.py
@@ -0,0 +1,13 @@
+import os
+
+for i in range(1,22):
+    command1 = "python3 repcrec.py tests/test" + str(i) + ".txt > output" + str(i) + ".txt"
+    # command2 = "diff a.txt refout/out" + str(i) + "_16_f"
+    # command2 = "~frankeh/Public/mmu -f128 -af -oOPFS inputs/in"+ str(i)+ " inputs/rfile > b.txt"
+    # command3 = "diff a.txt b.txt"
+    os.system(command1)
+    # os.system(command2)
+    # os.system(command3)
+
+os.system("python3 repcrec.py tests/test3.5.txt > output3.5.txt")
+os.system("python3 repcrec.py tests/test3.7.txt > output3.7.txt")
